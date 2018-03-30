0e6b688ef0cfee0615dd409c4a4bd12bc3e13723
eLn86 <elnathan.erh@gmail.com>
2018-03-30 11:25:04 +0800

    added weather API Key and Base API URI

--- server/config.js
+++ server/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   weather_api_KEY: 'ebc8583c1e110e04985667235be8ea3f',
-  weather_api_url: 'https://api.tfl.gov.uk/BikePoint',
-  weather_search_api_url: 'https://api.tfl.gov.uk/BikePoint/Search',
+  weather_api_url: 'http://api.openweathermap.org/data/2.5/',
   is_mock_data: false
 };  
