6579248929dcfec4e8e26ea6372c37f0542b047f
Beda Tse <beda@coeus.hk>
2018-04-11 13:46:19 +0800

    fix open weather map api key

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -12,7 +12,7 @@ let weatherController = {
     return res.status(200).send({ status: 'OK', version: '1.0' });
   },
   getFiveDaysWeather: (req, res, next) => {
-    const url = `${config.weather_api_url}forecast?q=London&${process.env.WEATHER_API_KEY}`
+    const url = `${config.weather_api_url}forecast?q=London&APPID=${process.env.WEATHER_API_KEY}`
     fetch(url)
     .then(response => {
       response.json().then(json => {
@@ -29,7 +29,7 @@ let weatherController = {
   getFiveDaysWeatherViaLatLong: (req, res, next) => {
     const latitude = req.params.lat
     const longitude = req.params.lon
-    const url = `${config.weather_api_url}forecast?lat=${latitude}&lon=${longitude}&${process.env.WEATHER_API_KEY}`
+    const url = `${config.weather_api_url}forecast?lat=${latitude}&lon=${longitude}&APPID=${process.env.WEATHER_API_KEY}`
     fetch(url)
     .then(response => {
       response.json().then(json => {
